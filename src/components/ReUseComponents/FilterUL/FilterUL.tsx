465b73dbefae324043a889808d8a90a3926da87c
geravvene <romanov17903@gmail.com>
2023-10-21 14:49:00 +0400

    select menu redisign

--- src/components/ReUseComponents/FilterUL/FilterUL.tsx
+++ src/components/ReUseComponents/FilterUL/FilterUL.tsx
@@ -1,5 +1,6 @@
 import cn from 'classnames/bind';
 import style from './filterUL.module.scss';
+import { useRef } from 'react';
 
 const cx = cn.bind(style);
 
@@ -10,13 +11,31 @@ interface IFilterUL {
   isDark: boolean;
 }
 
+const hoverFunction = (elem: HTMLLIElement, hoverElem: HTMLDivElement) => {
+  hoverElem.style.setProperty('height', `${elem.clientHeight}px`);
+  hoverElem.style.setProperty(
+    'margin-top',
+    `${
+      elem.getBoundingClientRect().y -
+      hoverElem.parentNode.getBoundingClientRect().y
+    }px`
+  );
+};
+
 function FilterUL({ data, onClick, filterName, isDark }: IFilterUL) {
+  const ref = useRef<HTMLDivElement>(null);
   return (
     <>
-      <div id="hover" className={cx('hover', { dark: isDark })} />
-      <ul className={cx('filter', { dark: isDark })}>
+      <div ref={ref} className={cx('hover', { dark: isDark })} />
+      <ul
+        className={cx('filter', { dark: isDark })}
+        onMouseLeave={() => ref.current.style.setProperty('height', "0px")}
+      >
         {data.map((item) => (
-          <li key={item.id}>
+          <li
+            key={item.id}
+            onMouseEnter={(e) => hoverFunction(e.currentTarget, ref.current!)}
+          >
             <button
               type="button"
               onClick={() => onClick(filterName, String(item.id))}
